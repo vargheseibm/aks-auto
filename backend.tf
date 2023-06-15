75407b173185b0d9540ad794f3ca60b9af8e9a2d
vargheese2022 <vargheese2022@outlook.com>
2023-06-15 16:54:18 +0200

    github

--- backend.tf
+++ backend.tf
@@ -1,8 +1,13 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "RG-ADA---CAKE-Team"
-    storage_account_name = "terstorage123"
-    container_name       = "terstatefile"
-    key                  = "terraform.tfstate"
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "RG-ADA---CAKE-Team"
+#     storage_account_name = "terstorage123"
+#     container_name       = "terstatefile"
+#     key                  = "terraform.tfstate"
+#   }
+# }
+
+
+storage_account_name = "terstorage123"
+container_name       = "terstatefile"
+key                  = "terraform.tfstate"
