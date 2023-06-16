47457a6206c5704b3317c4abd33b8e307e612f5f
vargheese2022 <vargheese2022@outlook.com>
2023-06-16 11:36:08 +0200

    updated

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -42,31 +42,28 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 }
 
 
-# resource "kubernetes_namespace_v1" "namespace" {
-#   metadata {
-#     name = "demo-application"
-#   }
-# }
-
-
-# resource "helm_release" "demo-app" {
-#   name       = "demo-application"
-#   repository = "https://viadee.github.io/springboot-helm-chart"
-#   chart      = "springboot-helm-chart"
-#   version    = "1.2.0"
-#   namespace  = kubernetes_namespace_v1.namespace.metadata[0].name
-#   set {
-#     name  = "service.type"
-#     value = "LoadBalancer"
-#   }
-#   set {
-#     name  = "autoscaling.enabled" #cannot be enabled because API is autoscaling/v2beta1 but our kubernetes only supports from autoscaling/v2beta2
-#     value = "false"
-#   }
-# }
-
-
-output "host" {
-    value = azurerm_kubernetes_cluster.k8s.kube_config
+resource "kubernetes_namespace_v1" "namespace" {
+  metadata {
+    name = "demo-application"
+  }
 }
+
+
+resource "helm_release" "demo-app" {
+  name       = "demo-application"
+  repository = "https://viadee.github.io/springboot-helm-chart"
+  chart      = "springboot-helm-chart"
+  version    = "1.2.0"
+  namespace  = kubernetes_namespace_v1.namespace.metadata[0].name
+  set {
+    name  = "service.type"
+    value = "LoadBalancer"
+  }
+  set {
+    name  = "autoscaling.enabled" #cannot be enabled because API is autoscaling/v2beta1 but our kubernetes only supports from autoscaling/v2beta2
+    value = "false"
+  }
+}
+
+
 
