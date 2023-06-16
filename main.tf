db0bd2a7536acf4e04a7f0dca7b1a19771738d78
vargheese2022 <vargheese2022@outlook.com>
2023-06-16 11:21:54 +0200

    updated

--- main.tf
+++ main.tf
@@ -4,3 +4,8 @@ module "aks" {
   resource_group_name = data.azurerm_resource_group.aks_rg.name
   location = data.azurerm_resource_group.aks_rg.location
 }
+
+
+output "host" {
+    value = module.aks.azurerm_kubernetes_cluster.k8s.host
+}
