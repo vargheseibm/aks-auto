d17b18c884da9272c1dda1da50aab00a26bd6e9f
vargheese2022 <vargheese2022@outlook.com>
2023-06-16 09:11:52 +0200

    updated

---
+++ modules/aks/variable.tf
@@ -0,0 +1,12 @@
+variable "cluster_name" {
+  type = string  
+  description = "cluster" 
+  default = "kubecluster-02"
+}
+
+
+variable "node_count" {
+  type = number  
+  description = "cluster" 
+  default = 1
+}
--- variable.tf
+++ variable.tf
@@ -20,23 +20,6 @@ variable "environment" {
 }
 
 
-variable "client_secret" {
-  type = string  
-  description = "secret" 
-  default = "1f18Q~9yaExFdagNeURPNXpEX_KK-yYLVe1zWc9q"
-}
 
 
 
-variable "cluster_name" {
-  type = string  
-  description = "cluster" 
-  default = "kubecluster-02"
-}
-
-
-variable "node_count" {
-  type = number  
-  description = "cluster" 
-  default = 1
-}
