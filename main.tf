34bbf4432766c619adba324d107c91d54ce0fd59
vargheese2022 <vargheese2022@outlook.com>
2023-06-19 10:50:47 +0200

    updated

--- main.tf
+++ main.tf
@@ -12,12 +12,12 @@ module "app-deploy" {
 }
 
 
-module "grafana0" {
-  source = "./modules/grafana0"
+# module "grafana0" {
+#   source = "./modules/grafana0"
 
-  resource_group_name = data.azurerm_resource_group.aks_rg.name
-  location = data.azurerm_resource_group.aks_rg.location
-}
+#   resource_group_name = data.azurerm_resource_group.aks_rg.name
+#   location = data.azurerm_resource_group.aks_rg.location
+# }
 
 
 # Datasource to get Latest Azure AKS latest Version
@@ -31,3 +31,26 @@ module "grafana0" {
   
 # }
 
+resource "azapi_resource" "azgrafana" {
+  type        = "Microsoft.Dashboard/grafana@2022-08-01" 
+  name        = "mgf-example"
+  parent_id   = data.azurerm_resource_group.aks_rg.id
+  location    = data.azurerm_resource_group.aks_rg.location
+
+  identity {
+    type      = "SystemAssigned"
+  }
+
+  body = jsonencode({
+    sku = {
+      name = "Standard"
+    }
+    properties = {
+      publicNetworkAccess = "Enabled",
+      zoneRedundancy = "Enabled",
+      apiKey = "Enabled",
+      deterministicOutboundIP = "Enabled"
+    }
+  })
+
+}
